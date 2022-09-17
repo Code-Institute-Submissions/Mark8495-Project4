9945a797b616571af4857ffc5e640afcd8d93ba6
Mark8495 <markbyrne8495@gmail.com>
2022-09-17 21:09:20 +0000

    added Recipe and comment model

--- recipes/models.py
+++ recipes/models.py
@@ -25,3 +25,60 @@ class Mealtime(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Recipe(models.Model):
+    " Django Model of recipe database "
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    preptime = models.ForeignKey(
+        Preptime,
+        on_delete=models.CASCADE,
+        related_name="recipe_posts"
+    )
+    mealtime = models.ForeignKey(
+        Mealtime,
+        on_delete=models.CASCADE,
+        related_name="recipe_posts"
+    )
+    author = models.ForeignKey(
+        User, 
+        on_delete=models.CASCADE, 
+        related_name="recipe_posts"
+    )
+    featured_image = CloudinaryField('image', default='placeholder')
+    updated_on = models.DateTimeField(auto_now=True)
+    serving_size = models.CharField(max_length=30, default=0)
+    ingredients = models.TextField(blank=True)
+    instructions = models.TextField(blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    likes = models.ManyToManyField(
+        User, 
+        related_name='recipe_likes', 
+        blank=True)
+
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return self.title
+
+    def number_of_likes(self):
+        return self.likes.count()
+
+class Comment(models.Model):
+    "Comments Model"
+    post = models.ForeignKey(Recipe, on_delete=models.CASCADE,
+                             related_name="comments")
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.name}"
