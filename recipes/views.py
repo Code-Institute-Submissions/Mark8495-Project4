41c193903be687c1a70ffc04a24b82ed140b170b
Mark8495 <markbyrne8495@gmail.com>
2022-09-18 20:46:32 +0000

    created recipe detail template

--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic, View
 from django.http import HttpResponseRedirect
-from .models import Post
+from .models import Recipe
 from .forms import CommentForm
+
+
+class RecipeList(generic.ListView):
+    model = Recipe
+    queryset = Recipe.objects.filter(status=1).order_by("-created_on")
+    template_name = "index.html"
+    paginate_by = 6
