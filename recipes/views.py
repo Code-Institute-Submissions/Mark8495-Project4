317875631abf213b68d840390014e70d9f84bcf5
Mark8495 <markbyrne8495@gmail.com>
2022-09-18 20:57:40 +0000

    created the paths for index view

--- recipes/views.py
+++ recipes/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic, View
 from django.http import HttpResponseRedirect
 from .models import Recipe
-from .forms import CommentForm
+# from .forms import CommentForm
 
 
 class RecipeList(generic.ListView):
@@ -10,3 +10,4 @@ class RecipeList(generic.ListView):
     queryset = Recipe.objects.filter(status=1).order_by("-created_on")
     template_name = "index.html"
     paginate_by = 6
+
