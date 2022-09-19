d1c030a9d39d96dd909848006b042fa69903856f
Mark8495 <markbyrne8495@gmail.com>
2022-09-19 20:36:23 +0000

    created recipe detail view

--- recipes/views.py
+++ recipes/views.py
@@ -11,3 +11,25 @@ class RecipeList(generic.ListView):
     template_name = "index.html"
     paginate_by = 6
 
+
+class RecipeDetail(View):
+
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Recipe.objects.filter(status=1)
+        recipe = get_object_or_404(queryset, slug=slug)
+        comments = recipe.comments.filter(approved=True).order_by("-created_on")
+        liked = False
+        if recipe.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        return render(
+            request,
+            "recipe_detail.html",
+            {
+                "recipe": recipe,
+                "comments": comments,
+                "commented": False,
+                "liked": liked,
+                "comment_form": CommentForm()
+            },
+        )
