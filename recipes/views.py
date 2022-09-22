64662a338bc5bc94d47f739dc5cfce112dc15465
Mark8495 <markbyrne8495@gmail.com>
2022-09-22 20:40:44 +0000

    added the post view for comments in views

--- recipes/views.py
+++ recipes/views.py
@@ -35,4 +35,34 @@ class RecipeDetail(View):
         )
     
 
-    
+    def post(self, request, slug, *args, **kwargs):
+
+        queryset = Recipe.objects.filter(status=1)
+        recipe = get_object_or_404(queryset, slug=slug)
+        comments = recipe.comments.filter(
+            approved=True).order_by("-created_on")
+        liked = False
+        if recipe.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            comment_form.instance.email = request.user.email
+            comment_form.instance.name = request.user.username
+            comment = comment_form.save(commit=False)
+            comment.post = recipe
+            comment.save()
+        else:
+            comment_form = CommentForm()
+
+        return render(
+            request,
+            "recipe_detail.html",
+            {
+                "recipe": recipe,
+                "comments": comments,
+                "commented": True,
+                "comment_form": comment_form,
+                "liked": liked
+            },
+        )
