fd8e2e221cd62173107c18a99f6646550ee13576
Mark8495 <markbyrne8495@gmail.com>
2022-10-01 18:21:38 +0000

    created recipe form

--- recipes/forms.py
+++ recipes/forms.py
@@ -1,7 +1,20 @@
-from .models import Comment
+from .models import Comment, Recipe
 from django import forms
 
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('body',)
+
+
+class RecipeForm(forms.ModelForm):
+    model = Recipe
+    fields = (
+        'title',
+        'mealtime',
+        'preptime',
+        'serving_size',
+        'ingredients',
+        'instructions',
+        'featured_image',
+    )
